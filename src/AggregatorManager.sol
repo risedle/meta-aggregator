479bbb2cc57060c0a78ac6929fa1369f92d49eb6
pyk <gm@pyk.sh>
2023-05-31 22:52:41 +0700

    feat(swap-router): Implement AggregatorManager

---
+++ src/AggregatorManager.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+import { Ownable } from "openzeppelin/access/Ownable.sol";
+
+contract AggregatorManager is Ownable {
+  /// @dev Event emitted if new dex aggregator is registered
+  event AggregatorRegistered(address indexed aggregator);
+
+  /// @dev Event emitted if existing dex aggregator is unregistered
+  event AggregatorUnregistered(address indexed aggregator);
+
+  /// @dev Error is raised if aggregator is not whitelisted
+  error AggregatorInvalid(address aggregator);
+
+  /// @dev Whitelisted aggregators
+  mapping(address aggregator => bool isWhitelisted) public aggregators;
+
+  /**
+   * @notice Register new dex aggregator
+   * @param aggregator Dex aggregator contract address (e.g. 1Inch)
+   */
+  function register(address aggregator) public onlyOwner {
+    aggregators[aggregator] = true;
+    emit AggregatorRegistered(aggregator);
+  }
+
+  /// @dev Modifier to make sure only whitelisted aggregator is called
+  modifier onlyWhitelistedAggregator(address aggregator) {
+    if (!aggregators[aggregator]) revert AggregatorInvalid(aggregator);
+    _;
+  }
+
+  /**
+   * @notice Unregister existing dex aggregator
+   * @param aggregator Existing dex aggregator contract address (e.g. 1Inch)
+   */
+  function unregister(address aggregator)
+    public
+    onlyOwner
+    onlyWhitelistedAggregator(aggregator)
+  {
+    aggregators[aggregator] = false;
+    emit AggregatorUnregistered(aggregator);
+  }
+}
--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
 
-contract Counter {
-  uint256 public number;
-
-  function setNumber(uint256 newNumber) public {
-    number = newNumber;
-  }
-
-  function increment() public {
-    number++;
-  }
-}
+import { Ownable } from "openzeppelin/access/Ownable.sol";
