889de8fb2dca683a0776346a065c93536cd8b50b
pyk <gm@pyk.sh>
2023-06-03 23:27:16 +0700

    feat: implement FeeCollector

--- test/TokenToETHSwapper.t.sol
+++ test/TokenToETHSwapper.t.sol
@@ -10,6 +10,7 @@ import { SafeERC20 } from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
 import { TokenToETHSwapper } from "src/TokenToETHSwapper.sol";
 import { AggregatorManager } from "src/AggregatorManager.sol";
+import { ISwapper } from "src/ISwapper.sol";
 import { SigUtils } from "./SigUtils.sol";
 
 /// @dev Mock aggregator that spend specified token and transfer specified amount
@@ -55,7 +56,6 @@ contract TokenToETHSwapperTest is Test {
     bytes memory data = abi.encodeWithSelector(
       Aggregator.swap.selector, tokenIn, tokenInAmount, amountOut
     ); // call data
-    uint256 fee = 0.001 ether; // 0.1%
 
     // Start swap as alice
     vm.startPrank(alice);
@@ -74,7 +74,7 @@ contract TokenToETHSwapperTest is Test {
       )
     );
     swapper.swapTokenToETH(
-      tokenIn, payable(address(aggregator)), data, tokenInAmount, amountOut, fee
+      tokenIn, payable(address(aggregator)), data, tokenInAmount, amountOut
     );
   }
 
@@ -98,7 +98,6 @@ contract TokenToETHSwapperTest is Test {
     bytes memory data = abi.encodeWithSelector(
       Aggregator.swap.selector, tokenIn, tokenInAmount, amountOut
     ); // call data
-    uint256 fee = 0.001 ether; // 0.1%
 
     // Start swap as alice
     vm.startPrank(alice);
@@ -112,7 +111,7 @@ contract TokenToETHSwapperTest is Test {
 
     // Do the swap
     swapper.swapTokenToETH(
-      tokenIn, payable(address(aggregator)), data, tokenInAmount, amountOut, fee
+      tokenIn, payable(address(aggregator)), data, tokenInAmount, amountOut
     );
 
     // Make sure alice receive correct amount of ETH
@@ -142,7 +141,6 @@ contract TokenToETHSwapperTest is Test {
     bytes memory data = abi.encodeWithSelector(
       Aggregator.maliciousSwap.selector, tokenIn, tokenInAmount
     ); // call data
-    uint256 fee = 0.001 ether; // 0.1%
 
     // Start swap as alice
     vm.startPrank(alice);
@@ -155,11 +153,9 @@ contract TokenToETHSwapperTest is Test {
     IERC20(tokenIn).safeApprove(address(swapper), tokenInAmount);
 
     // Do the swap
-    vm.expectRevert(
-      abi.encodeWithSelector(TokenToETHSwapper.ETHAmountInvalid.selector)
-    );
+    vm.expectRevert(abi.encodeWithSelector(ISwapper.ETHAmountInvalid.selector));
     swapper.swapTokenToETH(
-      tokenIn, payable(address(aggregator)), data, tokenInAmount, amountOut, fee
+      tokenIn, payable(address(aggregator)), data, tokenInAmount, amountOut
     );
   }
 
@@ -184,7 +180,6 @@ contract TokenToETHSwapperTest is Test {
     bytes memory data = abi.encodeWithSelector(
       Aggregator.swap.selector, tokenIn, tokenInAmount, 64986585
     ); // call data
-    uint256 fee = 0.001 ether; // 0.1%
 
     // Start swap as alice
     vm.startPrank(alice);
@@ -197,63 +192,10 @@ contract TokenToETHSwapperTest is Test {
     IERC20(tokenIn).safeApprove(address(swapper), tokenInAmount);
 
     // Do the swap
-    vm.expectRevert(
-      abi.encodeWithSelector(TokenToETHSwapper.ETHAmountInvalid.selector)
-    );
+    vm.expectRevert(abi.encodeWithSelector(ISwapper.ETHAmountInvalid.selector));
     swapper.swapTokenToETH(
-      tokenIn,
-      payable(address(aggregator)),
-      data,
-      tokenInAmount,
-      minAmountOut,
-      fee
+      tokenIn, payable(address(aggregator)), data, tokenInAmount, minAmountOut
     );
-  }
-
-  /// @dev Make sure the accounting is correct
-  function testSwapTokenToETHWithRegisteredAggregatorZeroFees() public {
-    // Deploy contract
-    TokenToETHSwapper swapper = new TokenToETHSwapper();
-    Aggregator aggregator = new Aggregator();
-
-    // Fund the aggregator
-    vm.deal(address(aggregator), 2e18);
-
-    // Register the aggregator
-    swapper.register(address(aggregator));
-
-    // Set input
-    address alice = address(101);
-    address tokenIn = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
-    uint256 tokenInAmount = 100 * 1e6; // 100 USDC
-    uint256 amountOut = 94986585;
-    bytes memory data = abi.encodeWithSelector(
-      Aggregator.swap.selector, tokenIn, tokenInAmount, amountOut
-    ); // call data
-    uint256 fee = 0;
-
-    // Start swap as alice
-    vm.startPrank(alice);
-
-    // Give alice tokenIn balance to swap
-    vm.store(tokenIn, keccak256(abi.encode(alice, 9)), bytes32(tokenInAmount));
-    assertEq(IERC20(tokenIn).balanceOf(alice), tokenInAmount);
-
-    // Approve swapper as token spender
-    IERC20(tokenIn).safeApprove(address(swapper), tokenInAmount);
-
-    // Do the swap
-    swapper.swapTokenToETH(
-      tokenIn, payable(address(aggregator)), data, tokenInAmount, amountOut, fee
-    );
-
-    // Make sure alice receive correct amount of ETH
-    uint256 totalFee = 0;
-    uint256 expectedAmount = 94986585 - totalFee;
-    assertEq(alice.balance, expectedAmount, "invalid alice ETH balance");
-    assertEq(address(swapper).balance, totalFee, "invalid swapper balance");
-
-    assertEq(IERC20(tokenIn).balanceOf(alice), 0, "invalid alice USDC balance");
   }
 
   /// @dev Make sure the accounting is correct
@@ -276,7 +218,6 @@ contract TokenToETHSwapperTest is Test {
     bytes memory data = abi.encodeWithSelector(
       Aggregator.swap.selector, tokenIn, tokenInAmount, amountOut
     ); // call data
-    uint256 fee = 0.001 ether; // 0.1%
 
     // Start swap as alice
     vm.startPrank(alice);
@@ -308,7 +249,6 @@ contract TokenToETHSwapperTest is Test {
       data,
       tokenInAmount,
       amountOut,
-      fee,
       block.timestamp + 1 days,
       v,
       r,
